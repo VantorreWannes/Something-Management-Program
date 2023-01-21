c706a7a08096f2b4823556607551b615ecfcea94
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-01-21 10:08:25 +0100

    Add project files.

---
+++ Something Management Program Remastered/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Something_Management_Program_Remastered
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
