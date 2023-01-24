22f7e6426736e19f592e6320aaeedd27913b3471
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-01-24 16:04:35 +0100

    Empty bug

---
+++ Something Management Program Remastered/Model/Modifier.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Something_Management_Program_Remastered.Model
+{
+    public enum Type { Add, Remove, Divide, Multiply}
+
+    public class Modifier
+    {
+        private string name;
+        private Type modType;
+        private string description;
+        private float amount;
+        private DateTime interval;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public Type ModType
+        {
+            get
+            {
+                return modType;
+            }
+            set
+            {
+                modType = value;
+                OnPropertyChanged("ModType");
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        public float Amount
+        {
+            get
+            {
+                return amount;
+            }
+            set
+            {
+                amount = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        public DateTime Interval
+        {
+            get
+            {
+                return interval;
+            }
+            set
+            {
+                interval = value;
+                OnPropertyChanged("Interval");
+            }
+        }
+
+
+        #region INotifyPropertyChanged Members  
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
---
+++ Something Management Program Remastered/Model/ObjectiveValue.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Something_Management_Program_Remastered.Model
+{
+    class ObjectiveValue
+    {
+        private string name;
+        private string type;
+        private string description;
+        private float amount;
+        private List<Modifier> modifiers;
+        private DateTime currentTime;
+
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = value;
+                OnPropertyChanged("Type");
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        public float Amount
+        {
+            get
+            {
+                return amount;
+            }
+            set
+            {
+                amount = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        public List<Modifier> Modifiers
+        {
+            get
+            {
+                return modifiers;
+            }
+            set
+            {
+                modifiers = value;
+                OnPropertyChanged("Modifiers");
+            }
+        }
+
+        public DateTime CurrentTime
+        {
+            get
+            {
+                return currentTime;
+            }
+            set
+            {
+                currentTime = value;
+                OnPropertyChanged("CurrentTime");
+            }
+        }
+
+        #region INotifyPropertyChanged Members  
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
